141755a5c66ac124e806e7c48b92554174c7cf20
sytnikov <sytnikov.alexey@gmail.com>
2023-06-09 17:08:45 +0300

    OOP, constructor functions, prototyping

--- main.js
+++ main.js
@@ -17,17 +17,20 @@ const toDo = [
     },
 ]
 
-for (let i = 0; i < toDo.length; i++) {
-    console.log(toDo[i].text);
-}
+// for (let i = 0; i < toDo.length; i++) {
+//     console.log(toDo[i].text);
+// }
 
-for (let x of toDo) {
-    console.log(x.text);
-}
+// for (let x of toDo) {
+//     console.log(x.text);
+// }
 
-toDo.forEach(function(todo) {
-    console.log(todo.text);
-});
+// toDo.forEach(function(todo) {
+//     console.log(todo.text);
+// });
+
+toDo.forEach((todo) => console.log(todo.text)); // using arrow function
+
 
 const toDoText = toDo.map(function(element) {
     return element.text;
@@ -40,3 +43,23 @@ const toDoCompleted = toDo.filter(function(element) {
     return element.text;
 })
 console.log(toDoCompleted);
+
+// Constructor function
+function Person (firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob); // in-built function within a constructor function
+    this.getBirthYear = () => this.dob.getFullYear(); // adding a method for an object
+    this.getFullName = () => `${this.firstName} ${this.lastName}`;
+}
+
+// Prototyping function: instead of having it in the object directly
+Person.prototype.getLastNameAndIntitals = function () {
+    return `${this.lastName} ${this.firstName[0]}.`;
+}
+
+//Instantiate person (an object)
+const person1 = new Person("Mary", "Smith", "04/06/1991");
+
+console.log(person1);
+console.log(person1.getLastNameAndIntitals());
